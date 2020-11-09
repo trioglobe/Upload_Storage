b5ec84a3b540caf4e8dbebd4166759adaee56065
AppMenu2 <appmenu2@gmail.com>
2020-11-09 19:46:43 +0530

    c

---
+++ app/src/main/java/com/trioglobe/developers_kit_demo/activity.java
@@ -0,0 +1,14 @@
+package com.trioglobe.developers_kit_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity);
+    }
+}
